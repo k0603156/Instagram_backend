f6d9f24c4d42efeb0994dcb051f36b3314d3a679
k0603156 <k0603156@gmail.com>
2019-08-27 13:51:25 +0900

    create GraphQL schema ,ADD morgan

--- src/server.js
+++ src/server.js
@@ -2,23 +2,14 @@ require('dotenv').config();
 import {
   GraphQLServer
 } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from "./schema";
 
 const PORT = process.env.PORT || 8001;
-const typeDefs = `
-  type Query{
-    hello: String!
-  }
-`
-const resolvers = {
-  Query: {
-    hello: () => "hi"
-  }
-};
-
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
-})
+  schema
+});
+server.express.use(logger('dev'));
 
 server.start({
   port: PORT
