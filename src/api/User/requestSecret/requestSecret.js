8e07f0c727fe15a844e1a770fa327bab887de737
k0603156 <k0603156@gmail.com>
2019-08-28 04:47:56 +0900

    api - requestSecret

---
+++ src/api/User/requestSecret/requestSecret.js
@@ -0,0 +1,30 @@
+import {
+  generateSecret
+} from "../../../utils";
+import {
+  prisma
+} from "../../../../generated/prisma-client";
+export default {
+  Mutation: {
+    requestSecret: async (_, args) => {
+      const {
+        email
+      } = args;
+      const loginSecret = generateSecret();
+      try {
+        await prisma.updateUser({
+          data: {
+            loginSecret: loginSecret
+          },
+          where: {
+            email: email
+          }
+        });
+        return true;
+      } catch {
+        return false;
+      }
+
+    }
+  }
+}
