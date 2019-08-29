c49fc57bc5abecfc9fd66b48cc1fbdf69c850878
k0603156 <k0603156@gmail.com>
2019-08-29 18:19:37 +0900

    nodemailer sendgrid

--- src/server.js
+++ src/server.js
@@ -2,9 +2,17 @@ require('dotenv').config();
 import {
   GraphQLServer
 } from 'graphql-yoga';
+import dotenv from 'dotenv';
+import path from 'path';
 import logger from 'morgan';
 import schema from "./schema";
+import {
+  sendSecretMail
+} from "./utils";
 
+dotenv.config({
+  path: path.resolve(__dirname, ".env")
+});
 const PORT = process.env.PORT || 8001;
 const server = new GraphQLServer({
   schema
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,35 @@ import {
   adjectives,
   nouns
 } from "./words";
+import nodeMailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({
+  path: path.resolve(__dirname, ".env")
+});
+
+const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodeMailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "k0603156@gmail.com",
+    to: address,
+    subject: "Login Secret for Prismagram",
+    html: `Hello! Your login secret it ${secret}. <br/> Copy paste on the app/website to login`
+  }
+  return sendMail(email);
+}
+
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
