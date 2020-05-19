40d89f8dc4f58cc80d9096c1990e14687b17fb9e
kimyongkuk <k0603156@gmail.com>
2020-05-20 00:53:54 +0900

    make devserver running with ngrok

--- src/server.js
+++ src/server.js
@@ -1,36 +1,51 @@
 import "./env";
-import {
-  prisma
-} from "../generated/prisma-client";
+import { prisma } from "../generated/prisma-client";
 import "./passport";
-import {
-  GraphQLServer
-} from 'graphql-yoga';
-import logger from 'morgan';
+import { GraphQLServer } from "graphql-yoga";
+import morgan from "morgan";
 import schema from "./schema";
-import {
-  authenticateJwt
-} from './passport';
-import {
-  isAuthenticated
-} from "./middlewares";
+import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 8001;
+const { ngrok, logger } =
+  process.env.NODE_ENV !== "production"
+    ? {
+        ngrok: require("ngrok"),
+        logger: morgan("dev"),
+      }
+    : {
+        ngrok: false,
+        logger: morgan("tiny"),
+      };
 
 const server = new GraphQLServer({
   schema,
-  context: ({
-    request
-  }) => ({
+  context: ({ request }) => ({
     db: prisma,
     request,
-    isAuthenticated
-  })
+    isAuthenticated,
+  }),
 });
 
-server.express.use(logger('dev'));
+server.express.use(logger);
 server.express.use(authenticateJwt);
 
-server.start({
-  port: PORT
-}, () => console.log(`Server running on port http://localhost:${PORT}`));
+server.start(
+  {
+    port: PORT,
+  },
+  async ({ port, endpoint, subscriptions, playground }) => {
+    console.log(`Server running on port http://localhost:${port}`);
+
+    if (ngrok) {
+      let url;
+      try {
+        url = await ngrok.connect(port);
+        console.log(`Server running with ngrok ${url}`);
+      } catch (e) {
+        return logger.error(e);
+      }
+    }
+  },
+);
